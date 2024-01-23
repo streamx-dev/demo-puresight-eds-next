476ac1b352979e57d8718ec2eb437fef5aff49b2
Tomasz Dziezyk <tomasz.dziezyk@ds.pl>
2024-01-23 09:54:39 +0100

    #DXP-451 Add cards list

---
+++ blocks/cards-list/cards-list.js
@@ -0,0 +1,35 @@
+import createCard from '../../components/card/card.js';
+
+const renderColumn = (columnCellsData) => {
+  const columFragment = document.createRange().createContextualFragment(`
+    <div class="column">
+      ${columnCellsData.map((cell) => createCard(cell).outerHTML).join('')}
+    </div>
+  `);
+
+  return columFragment.children[0];
+};
+
+export default async function decorate(block) {
+  const carouselSourceLink = block.textContent.trim();
+
+  let carouselData;
+
+  try {
+    carouselData = await (await fetch(carouselSourceLink)).json();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+
+    return;
+  }
+
+  const carouselFragment = document.createRange().createContextualFragment(`
+    <div class="columns is-tablet">
+      ${carouselData.map((column) => renderColumn(column).outerHTML).join('')}
+    </div>
+  `);
+
+  block.innerHTML = '';
+  block.append(carouselFragment.children[0]);
+}
