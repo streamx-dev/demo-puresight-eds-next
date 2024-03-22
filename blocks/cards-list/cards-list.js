755a8cb28e393dd1a3d7a478ca1852256e62f11e
Martyna Szeszko <martyna.szeszko@ds.pl>
2024-03-22 15:11:09 +0100

    DXP-714 change styling and html structure

--- blocks/cards-list/cards-list.js
+++ blocks/cards-list/cards-list.js
@@ -1,15 +1,5 @@
 import createCard from '../../components/card/card.js';
 
-const renderColumn = (columnCellsData) => {
-  const columFragment = document.createRange().createContextualFragment(`
-    <div class="column product-tiles">
-      ${columnCellsData.map((cell) => createCard(cell).outerHTML).join('')}
-    </div>
-  `);
-
-  return columFragment.children[0];
-};
-
 export default async function decorate(block) {
   const carouselSourceLink = block.textContent.trim();
 
@@ -23,24 +13,10 @@ export default async function decorate(block) {
 
     return;
   }
-  const items = carousel.values;
-  const splittedArray = [];
 
-  let rowsNumber = 4;
-  if (items.length % 5 === 0) {
-    rowsNumber = 5;
-  } else if (items.length % 3 === 0) {
-    rowsNumber = 3;
-  } else if (items.length % 4 === 0) {
-    rowsNumber = 4;
-  }
-
-  while (items.length > 0) {
-    splittedArray.push(items.splice(0, rowsNumber));
-  }
   const carouselFragment = document.createRange().createContextualFragment(`
-    <div class="columns is-tablet">
-      ${splittedArray.map((column) => renderColumn(column).outerHTML).join('')}
+    <div class="product-tiles">
+      ${carousel.values.map((cell) => createCard(cell).outerHTML).join('')}
     </div>
   `);
 
